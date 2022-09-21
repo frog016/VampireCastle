1b1d5eb3fd777e0ee19242499cff87761120ed73
frog016 <mixalyov.nikita@mail.ru>
2022-09-21 09:43:02 +0500

    Add wallets to Zenject

---
+++ Assets/Scripts/Installers/WalletInstaller.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu(fileName = "WalletInstaller", menuName = "Installers/WalletInstaller")]
+public class WalletInstaller : ScriptableObjectInstaller<WalletInstaller>
+{
+    [SerializeField] private MoneyWallet[] _wallets;
+
+    public override void InstallBindings()
+    {
+        foreach (var wallet in _wallets)
+            Container.BindInstance(wallet);
+    }
+}
