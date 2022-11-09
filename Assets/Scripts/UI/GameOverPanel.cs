605ce2db9fc9b25665df1aac6d7cfa2851897a83
Alex Suslov <suslo.2001@mail.ru>
2022-11-09 21:43:29 +0500

    UX/UI fix MoneyScore

--- Assets/Scripts/UI/GameOverPanel.cs
+++ Assets/Scripts/UI/GameOverPanel.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameOverPanel : PanelBase
 {
     [SerializeField] private Text _bestScoreText;
-    [SerializeField] private Text _moneyText;
+    [SerializeField] private TMP_Text _moneyText;
 
     public override void OpenPanel()
     {
         _bestScoreText.text = "Лучший счет: " + Statistic.Instance.BestScore;
         var value = (int)((GenerationManager.Instance.CurrentLevel - 1) * 1.5f);
         GoldenMoneyWallet.Instance.AddMoney(value);
-        _moneyText.text = "+ " + value;
+        _moneyText.text = "+ " + value + "     <voffset=0.25em> <size=20> <sprite=0> </size> </voffset>";
         base.OpenPanel();
     }
 
